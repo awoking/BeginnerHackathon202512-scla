fde57b6d0349344d90fe0ec43b0fe6ff68c6a307
s1hotarumaru <suteakaccc123@gmail.com>
2025-12-06 17:56:54 +0900

    task.py-import

--- backend/app/database/models/task.py
+++ backend/app/database/models/task.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime,Text
+from sqlalchemy import Column, Integer, String, DateTime,Text,Table, ForeignKey
+from sqlalchemy.orm import relationship
 from app.databae.session import Base #sessionで定義した親クラスを参照している。
 
 class Task(Base):
@@ -8,8 +9,14 @@ class Task(Base):
     time = Column(DateTime)
     description = Column(Text)
     deadline = Column(DateTime)
-        
 
+
+reders = Table(
+    "reders",
+    Base.metadate,
+    Column("task_id", Integer, ForeignKey("tasks.id")),
+    Column("userid", Integer, ForeignKey("user.id")),
+)
 
 
 
