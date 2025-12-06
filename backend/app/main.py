4f379b49601fae992325e26d1fcea38f40728bf9
s1hotarumaru <suteakaccc123@gmail.com>
2025-12-06 15:42:40 +0900

    main.py-routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from routers import auth, tasks, users, images #認証はauth,タスク管理はtask,ユーザー管理はuser,ファイルアップロードはimagesに記述する。
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+#ファイルを読み込む。
+app.include_router(auth)
+app.include_router(tasks)
+app.include_router(users)
+app.include_router(images)
