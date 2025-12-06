b004b14c224f1efbb0c502ea9924073040d591dc
s1hotarumaru <suteakaccc123@gmail.com>
2025-12-06 22:44:27 +0900

    Add login API

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,1 +1,41 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+#DB接続のための関数と型を呼び出す
+from app.database.session import get_db
+from app.models.user import User
+from app.schemas import UserCreate, UserRead
+
+
+#このファイル内のAPIは/usersから始める。
+#このファイル内のAPIには自動で/usersがつく。
+#タグをつけているのでSwagger UI（/docs）から確認可能。
+router = APIRouter(prefix="/users",tags={"users"})
+
+
+@router.post("/", response_model=UserRead)
+def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
+    #ユーザーの重複を判定
+    existing = db.query(User).filter(User.username == user_in.username).first()
+    #重複が存在した場合
+    if existing:
+        raise HTTPException(status_code=400,detail="ユーザー名はすでに使用されています")
+    
+    #ユーザー作成。今はそのままだけどあとでpasswordをbcrypt化します。
+    newuser = User{
+        username=user_in.username,
+        passord=user_in.password
+    }
+
+    #データベースに追加
+    db.add(newuser)
+    
+    #編集を反映させる
+    db.commit()
+    
+    #dbのデータでnewuserを更新する。
+    #この場合はpythonのnewuserにidの情報をつける。
+    db.refresh(newuser)
+
+    return newuser
 
--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -1,15 +1,17 @@
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime #datetimeは時間を扱うのに適した型。
+
 #共通項目
 class TaskBase(BaseModel):
     title:str
     description: str | None = None
     deadline: datetime | None = None
-    #descripiton,deadlineは送られてなくても問題ない
+    #descripiton,deadlineは送られてなくても問題ない。
 
 #入力用
 class TaskCreate(TaskBase):
     pass
+#TaskBaseと同じ型。ただしあとで入力の際の項目が増えるかもしれないのできちんと分けておく。
 
 #レスポンス用
 class TaskRead(TaskBase):
